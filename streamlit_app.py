333257bf190c01899f1a4cfd3db0f3afdc561174
MichaelS470 <139365317+MichaelS470@users.noreply.github.com>
2023-07-12 16:48:45 +0200

    Update streamlit_app.py
    Fixed stremlit to streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Favourites')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
-stremlit.text('🥑🍞Avocado Toast')
+streamlit.text('🥑🍞Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
